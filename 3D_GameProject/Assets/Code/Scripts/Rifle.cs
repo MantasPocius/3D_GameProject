31ea85bda595f3b971b4ea37e5a4b9eeac87d111
Daimytsu <161054989+Daimytsu@users.noreply.github.com>
2024-11-27 19:29:35 +0200

    Rifle code

---
+++ 3D_GameProject/Assets/Code/Scripts/Rifle.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+
+    public int maxAmmo = 10;
+    public int currentAmmo;
+    public float reloadTime = 2f;
+    public float fireRate = 1f;
+
+    public Camera playerCamera;
+
+    private bool isReloading = false;
+    private bool isReadyToFire = true;
+
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (Input.GetButtonDown("Fire1") && isReadyToFire)
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    void Shoot()
+    {
+        if (currentAmmo <= 0)
+        {
+            Debug.Log("No bullet");
+            return;
+        }
+
+        currentAmmo--;
+        isReadyToFire = false;
+
+        Debug.Log("Shot, bullet left " + currentAmmo);
+
+
+        RaycastHit hit;
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Object hit - " + hit.collider.name);
+
+
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemy hit");
+            }
+        }
+        else
+        {
+            Debug.Log("no hit");
+        }
+
+        Invoke(nameof(ResetFire), 1 / fireRate);
+    }
+
+    void ResetFire()
+    {
+        isReadyToFire = true;
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        Debug.Log("Reload");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo; 
+        isReloading = false;
+
+        Debug.Log("Reload complete");
+    }
+
+
+}
