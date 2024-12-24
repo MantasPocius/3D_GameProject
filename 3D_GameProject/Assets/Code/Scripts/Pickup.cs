97db912e874af4e98f9b63c1d1938b145d6a63cf
Anika-Lay <nomerty94@gmail.com>
2024-12-24 18:01:24 +0200

    Pickup Script + 3d models

---
+++ 3D_GameProject/Assets/Code/Scripts/Pickup.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Sprites;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Transform player; 
+    public float pickupRange = 2f; 
+    public string inventoryItemName = "The key"; 
+    public GameObject pickupHint; 
+    public GameObject pickupBackground; 
+
+    private bool isInRange = false;
+    public bool KeyPickedUp = false; 
+
+    void Update()
+    {
+        
+        if (Vector3.Distance(player.position, transform.position) < pickupRange)
+        {
+            if (!isInRange)
+            {
+                pickupHint.SetActive(true);
+                pickupBackground.SetActive(true);
+                isInRange = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                KeyPickedUp = true;
+
+                Debug.Log(inventoryItemName + " picked up!");
+
+                Destroy(gameObject);
+
+                pickupHint.SetActive(false);
+                pickupBackground.SetActive(false);
+            }
+        }
+        else
+        {
+            if (isInRange)
+            {
+                pickupHint.SetActive(false);
+                pickupBackground.SetActive(false);
+                isInRange = false;
+            }
+        }
+    }
+}
+
