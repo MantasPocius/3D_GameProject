10a6995272891d449100eb4ff1ee10093601de82
Aginime <pasha535353@gmail.com>
2025-01-03 13:49:33 +0200

    Light

--- 3D_GameProject/Assets/Code/Scripts/LightChange.cs
+++ 3D_GameProject/Assets/Code/Scripts/LightChange.cs
@@ -7,7 +7,7 @@ public class LightChange : MonoBehaviour
     [SerializeField] private Material skyBox;
     private static readonly int Exposure = Shader.PropertyToID("_Exposure");
     private float brightness = 0.84f;
-    
+    private bool change = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +19,50 @@ public class LightChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        skyBox.SetFloat(Exposure, brightness);
+        if (change)
+        {
+            Debug.Log("Asjerwiohj");
+            if (brightness == 0.84f)
+            {
+                Debug.Log("1");
+                while (brightness > 0)
+                {
+                    brightness -= 0.0001f;
+                    Debug.Log(brightness);
+                    skyBox.SetFloat(Exposure, brightness);
+                }
+            }
+            if (brightness == 0f)
+            {
+                Debug.Log("2");
+                while (brightness < 0.84f)
+                {
+                    brightness += 0.0001f;
+                    Debug.Log(brightness);
+                    skyBox.SetFloat(Exposure, brightness);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (tag == "Lighting Change")
+        {
+            change = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("ASDSAtgbrewu");
+        
+        if (tag == "Lighting Change")
+        {
+            change = true;
+        }
+
+
+        /*Debug.Log("ASDSAtgbrewu");
         if (tag == "Lighting Change")
         {
             Debug.Log("Asjerwiohj");
@@ -33,8 +71,10 @@ public class LightChange : MonoBehaviour
                 Debug.Log("1");
                 while (brightness > 0)
                 {
-                    brightness -= 0.02f;
-                    Debug.Log("8");
+                    brightness -= 0.0001f;
+                    Debug.Log(brightness);
+                    skyBox.SetFloat(Exposure, brightness);
+                    Update();
                 }
             }
             if (brightness == 0f)
@@ -42,9 +82,12 @@ public class LightChange : MonoBehaviour
                 Debug.Log("2");
                 while (brightness < 0.84f)
                 {
-                    brightness += 0.02f;
+                    brightness += 0.0001f;
+                    Debug.Log(brightness);
+                    skyBox.SetFloat(Exposure, brightness);
+                    Update();
                 }
             }
-        }
+        }*/
     }
 }
