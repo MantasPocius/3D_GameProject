0f9e605d3955edc6154c88f83eb9b9cd1fbeec78
Aginime <pasha535353@gmail.com>
2025-01-04 21:12:01 +0200

    DAHNESS

--- 3D_GameProject/Assets/Code/Scripts/LightChange.cs
+++ 3D_GameProject/Assets/Code/Scripts/LightChange.cs
@@ -2,81 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEditor;
 
 public class LightChange : MonoBehaviour
 {
     [SerializeField] private Light Light;
-    [SerializeField] private LightingSettings settings;
-    private static readonly int Intensity = Shader.PropertyToID("_Intensity_Multyplier");
-
-    private float brightness = 0.84f;
-    private bool change = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //settings.
-        //Debug.Log("ASDSA");
-        //Light.intensity = 1;
+        RenderSettings.ambientIntensity = 1;
+        Light.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (change && Light.enabled)
-        {
-            Light.enabled = false;
-        }
-        if (change && !Light.enabled)
-        {
-            Light.enabled = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (tag == "Lighting Change")
-        {
-            change = false;
-
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        if (tag == "Lighting Change")
+        StartCoroutine(Intensity(other));
+    }
+
+    IEnumerator Intensity(Collider other)
+    {
+        if (other.tag == "Lightness" && RenderSettings.ambientIntensity == 0f)
         {
-            change = true;
+            for (int i = 0; i <= 100; i += 1)
+            {
+                RenderSettings.ambientIntensity = (float)i / 100;
+                Light.intensity = (float)i / 100;
+                yield return new WaitForSeconds(0.01f);
+            }
         }
-
-
-        /*Debug.Log("ASDSAtgbrewu");
-        if (tag == "Lighting Change")
+        if (other.tag == "DAHKNESS" && RenderSettings.ambientIntensity == 1f)
         {
-            Debug.Log("Asjerwiohj");
-            if (brightness == 0.84f)
+            for (int i = 100; i >= 0; i -= 1)
             {
-                Debug.Log("1");
-                while (brightness > 0)
-                {
-                    brightness -= 0.0001f;
-                    Debug.Log(brightness);
-                    skyBox.SetFloat(Exposure, brightness);
-                    Update();
-                }
+                RenderSettings.ambientIntensity = (float)i / 100;
+                Light.intensity = (float)i / 100;
+                yield return new WaitForSeconds(0.01f);
             }
-            if (brightness == 0f)
-            {
-                Debug.Log("2");
-                while (brightness < 0.84f)
-                {
-                    brightness += 0.0001f;
-                    Debug.Log(brightness);
-                    skyBox.SetFloat(Exposure, brightness);
-                    Update();
-                }
-            }
-        }*/
+        }
     }
 }
